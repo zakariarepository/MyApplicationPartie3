884227e0912c9d3f97d17dc805ab266cae4fe8dd
Zakaria AIT <aitbenmesszakaria@gmail.com>
2023-01-05 19:46:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationpartie3/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplicationpartie3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void received(View view){
+
+        Intent intent = new Intent();
+        intent.setAction("FAKE_EVENT_INFO");
+
+        startActivity (intent);
+    }//
+
+}
---
+++ app/src/main/java/com/example/myapplicationpartie3/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.myapplicationpartie3;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Log.i("BR_TAG"," Event Received ");
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
